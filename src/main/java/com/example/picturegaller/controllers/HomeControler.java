682709bc68683cb0e39a6f0e4745e98cc840f827
Kinglsey <kinglsey@localhost.localdomain>
2025-03-26 15:31:58 +0000

    health check route

--- src/main/java/com/example/picturegaller/controllers/HomeControler.java
+++ src/main/java/com/example/picturegaller/controllers/HomeControler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeControler {
-    @GetMapping("/home") 
+    @GetMapping("/") 
     String home() {
         return "I am home";
     }
